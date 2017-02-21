3d9dc6fe0492a2f27b94a65b968c138bdc5cec0d
filipecosta90 <filipecosta.90@gmail.com>
2017-02-21 11:49:29 +0000

    preparing seqsort for mpi

--- seqsort.c
+++ seqsort.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <mpi.h>
 
 #define SIZE 100000000
 
-int *array;
+int* array;
 
+void choose_key(int size, int *i, int *j, int* sub_array){
+  //quicksort vars
+  int i=0,j=size-1,h;
+  int x=sub_array[size/2];
+  do
+  {    
+    while(sub_array[i]<x) {
+      i++;
+    }
+    while(sub_array[j]>x) {
+      j--;
+    }
+    if(i<=j)
+    {
+      h=sub_array[i]; sub_array[i]=sub_array[j]; sub_array[j]=h;
+      i++; j--;
+    }
+  } while(i<=j);
+}
 
 void quicksort(int lo, int hi)
 {
@@ -15,8 +35,12 @@ void quicksort(int lo, int hi)
   //partition
   do
   {    
-    while(array[i]<x) i++; 
-    while(array[j]>x) j--;
+    while(array[i]<x) {
+      i++;
+    }
+    while(array[j]>x) {
+      j--;
+    }
     if(i<=j)
     {
       h=array[i]; array[i]=array[j]; array[j]=h;
@@ -49,13 +73,38 @@ void validate() {
 }
 
 
-int main() {
+int main( int argc, char* argv[] ) {
+  int i,myid, numprocs = 4;
+  int num_elements_per_proc = SIZE / numprocs;
+  int source,count,dummy;
+  // buffer for pivots
+  int buffer[numprocs*3];
+  MPI_Status status;
+  MPI_Request request;
 
-  initialize();
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 
+
+
+  // Create a random array of elements on the root process.
+  if (myid == 0) {
+    initialize();
+  }
+
+  int* sub_nums = (int *)malloc(sizeof(int) * num_elements_per_proc);
+
+  MPI_Scatter(array, num_elements_per_proc, MPI_INT, subnums,
+      num_elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
+
+  int my_i,my_j;
   double time = omp_get_wtime();
-  quicksort(0,SIZE-1);
+  choose_key(num_elements_per_proc, &my_i, &my_j, subnums);
   printf("Time=%f\n", omp_get_wtime()-time);
+  //quicksort(0,SIZE-1);
+
+  MPI_Finalize();
 
   validate();
 
